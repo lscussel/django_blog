e2be66be9ee13a44edf9dacb7eda49eeffa8bccd
Leonardo Scussel <leonardoscussel@gmail.com>
2024-02-18 22:25:01 -0300

    worked on index site and put some dinamic code in index template view

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,36 @@
+from blog.models import Post
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+PER_PAGE = 9
 
 # Create your views here.
 def index(request):
-  return render(request, 'blog/pages/index.html')
+  posts = Post.objects.filter(is_published=True).order_by('-pk')
+  paginator = Paginator(posts, PER_PAGE)
+  page_number = request.GET.get("page")
+  page_obj = paginator.get_page(page_number)
+  content = {
+    'page_obj': page_obj
+  }
+  return render(request, 'blog/pages/index.html', content)
 
 def page(request):
-  return render(request, 'blog/pages/page.html')
+  change_here = ''
+  paginator = Paginator(change_here, PER_PAGE)
+  page_number = request.GET.get("page")
+  page_obj = paginator.get_page(page_number)
+  content = {
+    'page_obj': page_obj
+  }
+  return render(request, 'blog/pages/page.html', content)
 
 def post(request):
-  return render(request, 'blog/pages/post.html')
+  change_here = ''
+  paginator = Paginator(change_here, PER_PAGE)
+  page_number = request.GET.get("page")
+  page_obj = paginator.get_page(page_number)
+  content = {
+    'page_obj': page_obj
+  }
+  return render(request, 'blog/pages/post.html', content)
