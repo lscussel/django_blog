be95f61f5fe735e257e640d803baf06e8e569aa6
Leonardo Scussel <leonardoscussel@gmail.com>
2024-02-25 22:49:49 -0300

    installed django-axes

--- djangoapp/project/wsgi.py
+++ djangoapp/project/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dotenv import load_dotenv
+from pathlib import Path
+
+# DOTENV
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR.parent / 'dotenv_files' / '.env', override=True)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
